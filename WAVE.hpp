1fd777f09f807ae42675903e92673065132b79bc
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-25 21:25:26 +0530

    Added first few file.

---
+++ WAVE.hpp
@@ -0,0 +1,25 @@
+/***
+ *       Filename:  WAVEReader.hpp
+ *
+ *    Description:  WAVE file parser.
+ *
+ *        Version:  0.0.1
+ *        Created:  2017-11-25
+ *       Revision:  none
+ *
+ *         Author:  Dilawar Singh <dilawars@ncbs.res.in>
+ *   Organization:  NCBS Bangalore
+ *
+ *        License:  GNU GPL2
+ */
+
+class WAVE
+{
+    public:
+        WAVE();
+        ~WAVE();
+
+    private:
+        /* data */
+};
+   
---
+++ test_wav.cpp
@@ -0,0 +1,22 @@
+/***
+ *       Filename:  test_wav.cpp
+ *
+ *    Description:  Test the header file.
+ *
+ *        Version:  0.0.1
+ *        Created:  2017-11-25
+ *       Revision:  none
+ *
+ *         Author:  Dilawar Singh <dilawars@ncbs.res.in>
+ *   Organization:  NCBS Bangalore
+ *
+ *        License:  GNU GPL2
+ */
+
+#include "WAVE.hpp"
+
+int main(int argc, const char *argv[])
+{
+    
+    return 0;
+}
